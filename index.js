ad53e958f6ac6bd003904ab4ab092bb56aded1ac
120356aa <yurlovandrew@gmail.com>
2019-03-19 18:44:31 -0700

    indexjs setup

--- index.js
+++ index.js
@@ -11,16 +11,34 @@ server.use(helmet());
 server.unsubscribe(express.json());
 
 // GET all cohorts
+server.get('/api/cohorts', async (req, res) => {
+
+});
 
 // GET cohort by id
+server.get('/api/cohorts/:id', async (req, res) => {
+
+});
 
 // GET all students for cohort id
+server.get('/api/cohorts/:id/students', async (req, res) => {
+
+});
 
 // POST cohort
+server.post('/api/cohorts', async (req, res) => {
+
+});
 
 // PUT cohort
+server.put('/api/cohorts/:id', async (req, res) => {
+
+});
 
 // DELETE cohort
+server.delete('/api/cohorts/:id', async (req, res) => {
+
+});
 
 const port = process.env.PORT || 5000;
 server.listen(port);
